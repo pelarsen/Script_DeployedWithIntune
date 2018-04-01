728d3203e701781afa78f6ccc2322f34683f6859
PerLarsen1975 <34544411+PerLarsen1975@users.noreply.github.com>
2018-04-01 10:19:03 +0200

    Add files via upload

---
+++ EnableADAL on Onedrive.ps1
@@ -0,0 +1,11 @@
+﻿$registryPath = "HKCU:\SOFTWARE\Microsoft\OneDrive"
+$Name = "EnableADAL"
+$value = "1"
+IF(!(Test-Path $registryPath))
+  {
+    New-Item -Path $registryPath -Force | Out-Null
+    New-ItemProperty -Path $registryPath -Name $name -Value $value `
+    -PropertyType DWORD -Force | Out-Null}
+ ELSE {
+    New-ItemProperty -Path $registryPath -Name $name -Value $value `
+    -PropertyType DWORD -Force | Out-Null}
---
+++ EnableAutoConfig on Onedrive.ps1
@@ -0,0 +1,22 @@
+﻿$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive"
+$Name = "SilentAccountConfig"
+$value = "1"
+IF(!(Test-Path $registryPath))
+  {
+    New-Item -Path $registryPath -Force | Out-Null
+    New-ItemProperty -Path $registryPath -Name $name -Value $value `
+    -PropertyType DWORD -Force | Out-Null}
+ ELSE {
+    New-ItemProperty -Path $registryPath -Name $name -Value $value `
+    -PropertyType DWORD -Force | Out-Null}
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive"
+$Name = "FilesOnDemandEnabled"
+$value = "1"
+IF(!(Test-Path $registryPath))
+  {
+    New-Item -Path $registryPath -Force | Out-Null
+    New-ItemProperty -Path $registryPath -Name $name -Value $value `
+    -PropertyType DWORD -Force | Out-Null}
+ ELSE {
+    New-ItemProperty -Path $registryPath -Name $name -Value $value `
+    -PropertyType DWORD -Force | Out-Null}
