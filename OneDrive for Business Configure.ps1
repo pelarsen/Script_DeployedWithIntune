b29b01901388f977bb32e9be17121ab0d7f7ccba
MariusWeiss <39330414+MariusWeiss@users.noreply.github.com>
2018-08-28 08:59:15 +0200

    Updated version with function/hashtable

--- OneDrive for Business Configure.ps1
+++ OneDrive for Business Configure.ps1
@@ -10,77 +10,83 @@ This policy setup OneDrive For Business with AutoConfigure and KFM
     Author:      Per Larsen
     Created:     11-07-2018
     Product:     OneDrive for Business
-    Version:     1.0
+    Version:     1.1
     
 #>
 ###################################################################################################
 
 $TenantID = "TenantID"
 
+$OD4BSettings = @{
+    SilentAccountConfig = @{
+        registryPath  = "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive"
+        Name = 'SilentAccountConfig'
+        value = "1"
+        PropertyType = "DWORD"
+    }
+    FilesOnDemandEnabled = @{
+        registryPath  = "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive"
+        Name = 'FilesOnDemandEnabled'
+        value = "1"
+        PropertyType = "DWORD"
+    }
+    KFMBlockOptIn = @{
+        registryPath  = "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive"
+        Name = 'KFMBlockOptIn'
+        value = "1"
+        PropertyType = "DWORD"
+    }
+    KFMSilentOptIn = @{
+        registryPath  = "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive"
+        Name = 'KFMSilentOptIn'
+        value = $TenantID
+        PropertyType = "String"
+    }
+    KFMSilentOptInWithNotification = @{
+        registryPath  = "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive"
+        Name = 'KFMSilentOptInWithNotification'
+        value = "0"
+        PropertyType = "DWORD"
+    }
+    KFMBlockOptOut = @{
+        registryPath  = "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive"
+        Name = 'KFMBlockOptOut'
+        value = "1"
+        PropertyType = "DWORD"
+    }
+}
 
-$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive"
-$Name = "SilentAccountConfig"
-$value = "1"
-IF(!(Test-Path $registryPath))
-  {
-    New-Item -Path $registryPath -Force | Out-Null
-    New-ItemProperty -Path $registryPath -Name $name -Value $value `
-    -PropertyType DWORD -Force | Out-Null}
- ELSE {
-    New-ItemProperty -Path $registryPath -Name $name -Value $value `
-    -PropertyType DWORD -Force | Out-Null}
-$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive"
-$Name = "FilesOnDemandEnabled"
-$value = "1"
-IF(!(Test-Path $registryPath))
-  {
-    New-Item -Path $registryPath -Force | Out-Null
-    New-ItemProperty -Path $registryPath -Name $name -Value $value `
-    -PropertyType DWORD -Force | Out-Null}
- ELSE {
-    New-ItemProperty -Path $registryPath -Name $name -Value $value `
-    -PropertyType DWORD -Force | Out-Null}
 
-    $registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive"
-$Name = "KFMBlockOptIn"
-$value = "1"
-IF(!(Test-Path $registryPath))
-  {
-    New-Item -Path $registryPath -Force | Out-Null
-    New-ItemProperty -Path $registryPath -Name $name -Value $value `
-    -PropertyType DWORD -Force | Out-Null}
- ELSE {
-    New-ItemProperty -Path $registryPath -Name $name -Value $value `
-    -PropertyType DWORD -Force | Out-Null}
-$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive"
-$Name = "KFMSilentOptIn"
-IF(!(Test-Path $registryPath))
-  {
-    New-Item -Path $registryPath -Force | Out-Null
-    New-ItemProperty -Path $registryPath -Name $name -Value $TenantID `
-    -PropertyType String -Force | Out-Null}
- ELSE {
-    New-ItemProperty -Path $registryPath -Name $name -Value $TenantID `
-    -PropertyType String -Force | Out-Null}
-$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive"
-$Name = "KFMSilentOptInWithNotification"
-$value = "0"
-IF(!(Test-Path $registryPath))
-  {
-    New-Item -Path $registryPath -Force | Out-Null
-    New-ItemProperty -Path $registryPath -Name $name -Value $value `
-    -PropertyType DWORD -Force | Out-Null}
- ELSE {
-    New-ItemProperty -Path $registryPath -Name $name -Value $value `
-    -PropertyType DWORD -Force | Out-Null}
-$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive"
-$Name = "KFMBlockOptOut"
-$value = "1"
-IF(!(Test-Path $registryPath))
-  {
-    New-Item -Path $registryPath -Force | Out-Null
-    New-ItemProperty -Path $registryPath -Name $name -Value $value `
-    -PropertyType DWORD -Force | Out-Null}
- ELSE {
-    New-ItemProperty -Path $registryPath -Name $name -Value $value `
-    -PropertyType DWORD -Force | Out-Null}
+function Update-RegistryValue {
+    param (
+        [Parameter(Mandatory=$true)]
+        $registryPath,
+        [Parameter(Mandatory=$true)]
+        $Name,
+        [Parameter(Mandatory=$true)]
+        $value,
+        [Parameter(Mandatory=$true)]
+        $PropertyType
+    )
+
+    IF(!(Test-Path $registryPath)) {
+        New-Item -Path $registryPath -Force | Out-Null
+        New-ItemProperty -Path $registryPath -Name $Name -Value $value `
+        -PropertyType $PropertyType -Force | Out-Null}
+    ELSE {
+        New-ItemProperty -Path $registryPath -Name $Name -Value $value `
+        -PropertyType $PropertyType -Force | Out-Null
+    }
+    
+    
+}
+
+# Loop the hashtable...
+ForEach ($Setting in $OD4BSettings.Keys)
+{
+   Update-RegistryValue -registryPath $OD4BSettings[$Setting].registryPath `
+    -Name $OD4BSettings[$Setting].Name `
+    -value $OD4BSettings[$Setting].value `
+    -PropertyType $OD4BSettings[$Setting].PropertyType
+
+}
