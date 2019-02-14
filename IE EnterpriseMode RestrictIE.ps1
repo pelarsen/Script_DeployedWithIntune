e684b0aab732e229ffadec8a90cdd042fdeb7b66
Per Larsen [MVP] <34544411+PerLarsen1975@users.noreply.github.com>
2019-02-14 17:27:31 +0100

    Add files via upload

--- IE EnterpriseMode RestrictIE.ps1
+++ IE EnterpriseMode RestrictIE.ps1
@@ -1,4 +1,20 @@
-﻿$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer\Main\EnterpriseMode"
+﻿##################################################################################################
+<#
+.SYNOPSIS
+Policy Definition template file for Intune to run with the Microsoft Intune custome profile
+This policy setup OneDrive For Business with AutoConfigure and KFM 
+
+
+.NOTES
+    FileName:    IE EnterpriseMode RestrictIE.ps1
+    Author:      Per Larsen
+    Created:     14-02-2019
+    Product:     Internet Explorer
+    Version:     1.0
+    
+#>
+###################################################################################################
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer\Main\EnterpriseMode"
 
 #Send all sites not included in the Enterprise Mode Site List to Microsoft Edge.
 $Name = "RestrictIE"
