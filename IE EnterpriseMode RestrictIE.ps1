22998b09ff6da37a58e6db88970cf4ac7ada652a
Per Larsen [MVP] <34544411+PerLarsen1975@users.noreply.github.com>
2019-02-14 17:25:58 +0100

    Add files via upload

---
+++ IE EnterpriseMode RestrictIE.ps1
@@ -0,0 +1,12 @@
+﻿$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer\Main\EnterpriseMode"
+
+#Send all sites not included in the Enterprise Mode Site List to Microsoft Edge.
+$Name = "RestrictIE"
+$value = "1"
+IF((Test-Path -LiteralPath $registryPath) -ne $true) 
+{  New-Item $registryPath -force -ea SilentlyContinue };
+New-ItemProperty -LiteralPath $registryPath -Name $Name -Value $value -PropertyType DWord -Force -ea SilentlyContinue;
+
+
+
+  
