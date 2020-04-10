997a82388dab47e8f9713b31f26b03058ed7c679
Per Larsen <pelarsen@microsoft.com>
2020-04-10 10:17:09 +0200

    Add files via upload
    Enabled-ReservedStorage.ps1
    Script to test if reserved storage is disabled if true then enabled 
    Disabled-ReservedStorage.ps1
    Script to test if reserved storage is enabled if true then disabled

---
+++ Disabled-ReservedStorage.ps1
@@ -0,0 +1,6 @@
+﻿ $Storage = Invoke-Command {DISM /Online /Get-ReservedStorageState}  
+ $Output = $Storage | Select-String "Reserved storage is enabled."  
+ Write-Host $Output  
+ If ($Output -like "Reserved storage is enabled."){  
+      Invoke-Command {DISM /Online /Set-ReservedStorageState /State:Disabled}   
+ }  
---
+++ Enabled-ReservedStorage.ps1
@@ -0,0 +1,6 @@
+﻿ $Storage = Invoke-Command {DISM /Online /Get-ReservedStorageState}  
+ $Output = $Storage | Select-String "Reserved storage is disabled."  
+ Write-Host $Output  
+ If ($Output -like "Reserved storage is disabled."){  
+      Invoke-Command {DISM /Online /Set-ReservedStorageState /State:Enabled}   
+ }  
